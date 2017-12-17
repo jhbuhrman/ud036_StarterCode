02a0ebfee45effdf7cc6aa936d6b25944e9ad205
Jan-Hein Bührman <buhrman@xs4all.nl>
2017-12-17 17:17:26 +0100

    Added function docstrings and changed global constants to uppercase

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,10 +1,14 @@
+
+# This file is suitable for python3
+
+
 import webbrowser
 import os
 import re
 
 
 # Styles and scripting for the page
-main_page_head = '''
+MAIN_PAGE_HEAD = '''
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -87,7 +91,7 @@ main_page_head = '''
 
 
 # The main page layout and title bar
-main_page_content = '''
+MAIN_PAGE_CONTENT = '''
   <body>
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
@@ -121,7 +125,7 @@ main_page_content = '''
 
 
 # A single movie entry html template
-movie_tile_content = '''
+MOVIE_TILE_CONTENT = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
@@ -130,6 +134,19 @@ movie_tile_content = '''
 
 
 def create_movie_tiles_content(movies):
+    """Create the HTML content for all movie tiles.
+
+    Paramters:
+    movies: an iterable of Movie instances
+
+    each Movie instance should contain at least the following items:
+    * movie_title: the title of the movie
+    * poster_image_url: a URL pointing to movie's the poster image
+    * trailer_youtube_url: a URL pointing to the movie's Youtube trailer
+
+    It returns the HTML content displaying the movie tiles.
+    """
+    
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
@@ -142,7 +159,7 @@ def create_movie_tiles_content(movies):
                               else None)
 
         # Append the tile for the movie with its content filled in
-        content += movie_tile_content.format(
+        content += MOVIE_TILE_CONTENT.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
@@ -151,15 +168,20 @@ def create_movie_tiles_content(movies):
 
 
 def open_movies_page(movies):
-    # Create or overwrite the output file
+    """Create (or overwrite) an htnl file displaying all the movie tiles.
+
+    It takes a list of movies that serves as input for the rendering.
+    See also the description of the create_movie_tiles_content function.
+    """
+    
     output_file = open('fresh_tomatoes.html', 'w')
 
     # Replace the movie tiles placeholder generated content
-    rendered_content = main_page_content.format(
+    rendered_content = MAIN_PAGE_CONTENT.format(
         movie_tiles=create_movie_tiles_content(movies))
 
     # Output the file
-    output_file.write(main_page_head + rendered_content)
+    output_file.write(MAIN_PAGE_HEAD + rendered_content)
     output_file.close()
 
     # open the output file in the browser (in a new tab, if possible)
